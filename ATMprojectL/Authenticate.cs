b95811d4776bdeb2e2d1032cdfaf9f569f631ade
MihaelaGuja <mihaelaguja199@gmail.com>
2024-05-14 17:42:20 +0300

    file formatting

---
+++ ATMprojectL/Authenticate.cs
@@ -0,0 +1,115 @@
+﻿namespace ATMbank
+{
+    class Authenticate
+    {
+        private static Dictionary<string, string> _authentication { get; set; }
+        public Dictionary<string,string> Authentication
+        {
+            get {return _authentication;}
+            set 
+            {
+                foreach(var keyValuePair in value)
+                {
+                    if(keyValuePair.Value.Length > 10)
+                    {
+                        Console.WriteLine("Enter below 10 characters");
+                    }
+                }
+                _authentication = value;
+            }
+        }
+
+
+        public Authenticate()
+        {
+            Authentication = new Dictionary<string, string> { {"qwerty", "qwerty" } };
+        }
+        public bool changePassword()
+        {
+            Console.WriteLine("What is you username");
+            string username = Console.ReadLine();
+
+            if (!Authentication.ContainsKey(username))
+            {
+                Console.WriteLine("Username is incorrect");
+            }
+
+            Console.WriteLine("Enter old password: ");
+            string oldPassword = Console.ReadLine();
+
+            if(!Authentication.ContainsValue(oldPassword))
+            {
+                Console.WriteLine("Password is incorrect");
+            }
+
+            if (Authentication[username] == oldPassword)  
+            {
+                Console.WriteLine("Enter you new password: ");
+                string newPassword = Console.ReadLine();
+
+                Authentication[username] = newPassword;
+                Console.WriteLine($"pasword changed! {newPassword}");
+                return true;
+            }
+            else
+            {
+                Console.ReadKey();
+                Console.Clear();
+                return changePassword();
+            }
+            
+        }
+        public void Register()
+        {
+            Console.Write("Insert new username: ");
+            string username = Console.ReadLine();
+
+            if (Authentication.ContainsKey(username) )
+            {
+                Console.WriteLine("Username alredy exists!");
+                return;
+            }
+
+            Console.WriteLine("Insert new password: ");
+            string password = Console.ReadLine();
+
+            if (Authentication.ContainsValue(password))
+            {
+                Console.WriteLine("password alredy exists!");
+                return;
+            }
+            else
+            {
+                Authentication.Add(username, password);
+                Console.WriteLine("Successful registration");
+            }
+            
+
+        }
+        public bool LoginToAccount()
+        {
+            Console.WriteLine("=== Login ===");
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            if (!_authentication.ContainsKey(username))
+            {
+                Console.WriteLine("username incorrect or doesn't exist!");
+                return false;
+            }
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            
+            if (!_authentication.ContainsValue(password))
+            {
+                Console.WriteLine("password incorrect or doesn't exist!");
+                return false;
+            }
+
+           return _authentication[username] == password;
+            
+        }
+    }
+}
---
+++ ATMprojectL/BankATM.cs
@@ -0,0 +1,64 @@
+﻿namespace ATMbank
+{
+    class BankATM 
+    {
+        private double _balance;
+        public double Balance
+        {
+            get { return _balance; }
+            private set
+            {
+                if (value >= 0)
+                {
+                    _balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Balance cannot be negative.");
+                }
+            }
+        }
+        public void ExtractMoney()
+        {
+            Console.WriteLine("Enter amount of money that you want to extract: ");
+
+            if(!double.TryParse(Console.ReadLine(), out double extractionAmount))
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            Balance -= extractionAmount;
+            Console.WriteLine($"You balance is: {Balance}");
+        }
+
+        public void AddMoney()
+        {
+            Console.WriteLine("Enter amount of money that you want to add: ");
+
+            if(!double.TryParse (Console.ReadLine(), out double addToBalance))
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+
+            Balance += addToBalance;
+            Console.WriteLine($"Your balance is: {Balance}");
+        }
+
+        public void ViewBalance()
+        {
+            Console.WriteLine($"Balance on this acount is: {Balance}");
+        }
+
+        public void PrintOptions()
+        {
+            Console.WriteLine("ATM Options: ");
+            Console.WriteLine();
+            Console.WriteLine("1. View account balance " +
+                "\n2. Add money to account balance." +
+                "\n3. Extract money from account.." +
+                "\n4. Change account password." +
+                "\n0. Exit Menu");
+        }
+    }
+}
